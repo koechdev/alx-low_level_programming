3181b1fee27d374af590c8e6288eea78c73dce6b
KIRWAMC <kirwamarum@gmail.com>
2023-02-16 07:25:26 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints the size of various computer types
+ * Return 0 (success)
+ */
+int main(void)
+{char a;
+int b;
+long long int d;
+float f;
+
+printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(a));		
