3139e952561ad194feb4433a3fc863db26032fea
KIRWAMC <kirwamarum@gmail.com>
2023-02-21 09:48:10 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *  print_alphabet - print all alphabet in lowercase
+ *  print_alphabet - print all alphabet in lowercase,followed by a new line
  */
 
 void print_alphabet(void);
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
 	_putchar('\n');
 }
