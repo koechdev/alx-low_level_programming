be2d6b11a4712c9c34067a61b6d3c3cb2a6bc423
KIRWAMC <kirwamarum@gmail.com>
2023-03-11 00:19:31 -0800

    ./mul 2 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,10 +3,11 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
+ *  @s: string to be converted
  *
- * Return: the int converted from the string
+ *  Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -22,17 +23,25 @@ int _atoi(char *s)
 		len++;
 	while (i < len && f == 0)
 	{
-		digit = s[i] - '0';
-		if (d % 2)
-			digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
-		f = 0;
-	}
+		if (s[i] == '-')
+			++d;
 
-	i++;
-	return (0);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+
+		return (0);
 	return (n);
 }
