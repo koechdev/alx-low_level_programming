dacab37b0a4d0af8ed217f736df90826d6190034
KIRWAMC <kirwamarum@gmail.com>
2023-05-08 08:35:58 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,25 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
-
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
+ *
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
+	char *buffer = malloc(sizeof(char) * 1024);
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -29,15 +24,14 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
+ *
  * @fd: The file descriptor to be closed.
  */
 void close_file(int fd)
 {
-	int c;
+	int result = close(fd);
 
-	c = close(fd);
-
-	if (c == -1)
+	if (result == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -46,6 +40,7 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
+ *
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -72,11 +67,11 @@ int main(int argc, char *argv[])
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
+	while (r > 0)
+	{
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -84,16 +79,13 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
 		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
+	}
 
 	free(buffer);
 	close_file(from);
@@ -101,3 +93,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
