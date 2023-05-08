276a85fb58507ee517429351c072e3f4e85d0c2c
KIRWAMC <kirwamarum@gmail.com>
2023-05-08 03:57:34 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,22 +11,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int file_descriptor, bytes_written, text_length = 0;
+	int o, w, len = 0;
 
-        if (filename == NULL)
+	if (filename == NULL)
+		return (-1);
 
-                return (-1);
-        if (text_content != NULL)
-        {
-                while (text_content[text_length])
-                        text_length++;
-        }
-        file_descriptor = open(filename, O_WRONLY | O_APPEND);
-        if (file_descriptor == -1)
-                return (-1);
-        bytes_written = write(file_descriptor, text_content, text_length);
-        if (bytes_written == -1)
-                return (-1);
-        close(file_descriptor);
-        return (1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
+
