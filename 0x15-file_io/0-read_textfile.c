62ad53ad46ffdac0b869c9ca7dcedb692f76a1a1
KIRWAMC <kirwamarum@gmail.com>
2023-05-08 07:47:57 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,57 +1,51 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * read_and_print_file - reads a text file and prints it to STDOUT
- * @filename: name of file to be read
- * @num_bytes: number of bytes to be read from the file
+ * read_textfile - reads and prints the contents of a text file to STDOUT
+ * @filename: the name of the file to read
+ * @letters: the maximum number of letters to read and print
  *
- * Return: actual number of bytes read and printed, or 0 if function fails
+ * Return: the actual number of bytes read and printed, or 0 if an error occurs
  */
-ssize_t read_and_print_file(const char *filename, size_t num_bytes)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
 	char *buffer;
-	ssize_t bytes_read, bytes_written, total_bytes = 0;
+	ssize_t file_descriptor, bytes_read, bytes_written;
 
-	if (filename == NULL)
+	if (!filename) /* check if filename is NULL */
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * num_bytes);
-	if (buffer == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer) /* check if malloc fails */
 	{
 		close(file_descriptor);
 		return (0);
 	}
 
-	bytes_read = read(file_descriptor, buffer, num_bytes);
+	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
-		close(file_descriptor);
 		free(buffer);
+		close(file_descriptor);
 		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		close(file_descriptor);
 		free(buffer);
+		close(file_descriptor);
 		return (0);
 	}
-
-	total_bytes = bytes_written;
 
 	free(buffer);
 	close(file_descriptor);
 
-	return (total_bytes);
+	return (bytes_written);
 }
 
