5615e10060b712ad432232e953fce45f20453921
KIRWAMC <kirwamarum@gmail.com>
2023-05-08 08:11:23 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,57 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * create_file - Creates a new file and writes text to it.
+ * @filename: The name of the file to create.
+ * @text_content: The text to write to the file.
  *
- * @filename: pointer to the name of the file to be read
- * @letters: number of letters to read and print
- *
- * Return: the actual number of letters read and printed, 0 if function fails
+ * Return: On success, 1.
+ * On failure, -1.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	char *buf;
-	ssize_t file_descriptor, write_return, read_return, total_bytes_read;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-		return (0);
+	return (-1);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(file_descriptor);
-		return (0);
-	}
-
-	read_return = read(file_descriptor, buf, letters);
-	if (read_return == -1)
-	{
-		free(buf);
-		close(file_descriptor);
-		return (0);
-	}
-
-	write_return = write(STDOUT_FILENO, buf, read_return);
-	if (write_return == -1 || write_return != read_return)
-	{
-		free(buf);
-		close(file_descriptor);
-		return (0);
-	}
-
-	total_bytes_read = (ssize_t)read_return;
-
-	free(buf);
-	close(file_descriptor);
-
-	return (total_bytes_read);
+if (text_content != NULL)
+{
+	while (text_content[len])
+		len++;
 }
 
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
+	return (-1);
+
+w = write(fd, text_content, len);
+if (w == -1)
+{
+	close(fd);
+	return (-1);
+}
+
+close(fd);
+
+return (1);
+}
